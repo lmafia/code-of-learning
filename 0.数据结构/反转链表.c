59618418bafb98e767b2769cc2d21516519e1db1
lmafia <296427390@qq.com>
2019-09-06 10:28:57 +0800

    Signed-off-by: lmafia <296427390@qq.com>

--- 0.数据结构/反转链表.c
+++ 0.数据结构/反转链表.c
@@ -4,21 +4,21 @@ typedef struct list_node
     struct list_node *next;
 }list_node, *list;
 
-list reverse_list(list phead)
+ListNode *reverse_list(ListNode *pHead)
 {
-    if(!phead) /*空结点传入*/
+    if(!pHead) /*空结点传入*/
         return NULL;
 
-    if(!phead->next) /*只有一个结点传入*/
-        return phead;
+    if(!pHead->next) /*只有一个结点传入*/
+        return pHead;
 
                      /*多个结点传入*/
-    list p_rhead = NULL; /*来指向反转链表*/
-    list p_node = phead; /*当前结点*/
-    list p_pre = NULL; /*前驱结点*/
+    ListNode *p_rhead = NULL; /*来指向反转链表*/
+    ListNode *p_node = pHead; /*当前结点*/
+    ListNode *p_pre = NULL; /*前驱结点*/
     while(p_node != NULL)
     {
-        list p_next = p_node->next; /*先把后继保存了*/
+        ListNode *p_next = p_node->next; /*先把后继保存了*/
         if(p_next == NULL)          /*走到旧链表的最后一个结点，作为新的头结点*/
             p_rhead = p_node;
         p_node->next = p_pre;        /*当前结点的后继指向前驱*/
@@ -26,4 +26,30 @@ list reverse_list(list phead)
         p_node = p_next;             /*把当前结点移到后继结点*/
     }
     return p_rhead;
+}
+
+ListNode *reverse_list2(ListNode *pHead)
+{
+	if(pHead == NULL || pHead->next == NULL)
+		return pHead;
+	ListNode *p_now = pHead;
+	ListNode *re_pHead = NULL;
+	while(p_now != NULL)
+	{
+		ListNode *p_next = p_now->next;
+		p_now->next = re_pHead;
+		re_pHead = p_now;
+		p_now = p_next;
+	}
+	return re_pHead;
+}
+
+ListNode *reverse_list3(ListNode *pHead)
+{
+	if(pHead == NULL || pHead->next == NULL)
+		return pHead;
+	ListNode *re_pHead = reverse_list3(pHead->next);
+	pHead->next->next = pHead;
+	pHead->next = NULL;
+	return re_pHead;
 }
---
+++ 4.c++and刷题/0.test/0.test/0.test.cpp
@@ -0,0 +1,14 @@
+// 0.test.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+
+	int i = 0;
+	
+    return 0;
+}
+
